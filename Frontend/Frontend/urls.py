f57b288637d6bb888b4051ce4441c675452d9e10
Eva Strelkova <133266468+Strevochka@users.noreply.github.com>
2023-12-13 14:10:49 +0300

    Update urls.py

--- Frontend/Frontend/urls.py
+++ Frontend/Frontend/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+print("Hello")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
